f38f8eb90e43fd937886bb80c2a9083f00bdd0ee
Amit Aryeh Levy <amit@amitlevy.com>
2014-12-10 23:36:48 -0800

    Use USART in init
    * Move out initial task to own module
    
    * Print out some fun ascii art to UART
    
    * Minimal power management implementation to get USART clock working

--- src/main.rs
+++ src/main.rs
@@ -16,48 +16,15 @@ mod nvic;
 mod task;
 mod timer;
 mod lang_items;
+mod init;
 mod ringbuf;
 mod usart;
+mod pm;
 pub mod support;
-
-static LED : gpio::Pin = gpio::Pin { bus : gpio::Port::PORT2, pin: 10 };
-
-fn set_led() {
-    LED.set();
-    timer::set_alarm(1 << 16, set_led);
-}
-
-fn clear_led() {
-    LED.clear();
-    timer::set_alarm(1 << 16, clear_led);
-}
-
-fn app_entry() {
-    LED.make_output();
-    LED.set();
-
-    let uart = usart::USART::UART3;
-    uart.init_uart();
-    uart.set_baud_rate(38400);
-    uart.enable_tx();
-
-    while !uart.tx_ready() {}
-    uart.send_byte('a' as u8);
-
-    while !uart.tx_ready() {}
-    uart.send_byte('b' as u8);
-
-    LED.clear();
-
-    timer::setup();
-
-    timer::set_alarm(1 << 15, set_led);
-    timer::set_alarm(1 << 16, clear_led);
-}
 
 #[no_mangle]
 pub extern fn main() -> int {
-    task::post(app_entry);
+    task::post(init::init);
     loop {
       use core::option::{None, Some};
       match unsafe { task::dequeue() } {
