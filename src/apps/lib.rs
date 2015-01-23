c90f18086f993822809188d29192e44bd2976db4
Sergio Benitez <sbenitez@mit.edu>
2015-01-23 14:25:48 -0800

    Split blink module from app library.

--- src/apps/lib.rs
+++ src/apps/lib.rs
@@ -9,6 +9,8 @@ extern crate hil;
 
 use core::prelude::*;
 
+pub mod blink;
+
 #[allow(improper_ctypes)]
 extern {
     fn __subscribe(driver_num: usize, arg1: usize, arg2: usize) -> isize;
@@ -30,36 +32,3 @@ fn toggle_led() {
         __command(1, 0, 0);
     }
 }
-
-pub mod blinkapp {
-    static mut count: usize = 0;
-
-    #[inline(never)]
-    pub fn initialize() {
-        super::toggle_led();
-        super::writeln("I'm in the app!");
-
-        unsafe {
-            super::__subscribe(0, 1 << 15, timer_fired as usize);
-            super::__wait();
-        }
-    }
-
-    #[inline(never)]
-    pub fn timer_fired() {
-        super::toggle_led();
-
-        unsafe {
-            count += 1;
-            if count % 10 == 0 {
-                super::writeln("Timer fired 10 times");
-            }
-        }
-
-        unsafe {
-            super::__subscribe(0, 1 << 15, timer_fired as usize);
-            super::__wait();
-        }
-    }
-}
-
