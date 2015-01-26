2fc01af11732fc01670775d249da3d8de01af17b
Sergio Benitez <sbenitez@mit.edu>
2015-01-26 15:33:55 -0800

    Fully working extern C-app loading.

--- src/main.rs
+++ src/main.rs
@@ -21,46 +21,41 @@ mod task;
 mod ringbuf;
 pub mod syscall;
 
-static mut PROCESS_STACK: [usize; 4096] = [0; 4096];
+static mut PSTACKS: [[usize; 256]; 16] = [[0; 256]; 16];
 
 extern {
-    fn __start_apps();
+    static _sapps: u32;
+    static _eapps: u32;
+}
+
+unsafe fn schedule_external_apps() {
+    let (start_ptr, end_ptr) = (&_sapps as *const u32, &_eapps as *const u32);
+
+    let mut ptr = start_ptr;
+    while ptr < end_ptr {
+        task::Task::UserTask(*ptr as usize).post();
+        ptr = ptr.offset(1);
+    }
+}
+
+fn launch_task(task: task::Task) {
+    match task {
+        task::Task::UserTask(task_addr) => unsafe {
+            syscall::switch_to_user(task_addr, &mut PSTACKS[0][255]);
+        },
+        task::Task::KernelTask(task) => {
+            task();
+        }
+    }
 }
 
 #[no_mangle]
 pub extern fn main() {
-    use task;
-    use task::Task::*;
-
-    /* use drivers::flash_attr::FlashAttr;
-    spi::set_mode(spi::MSTR::Master, spi::PS::Variable,
-                      spi::RXFIFO::Disable, spi::MODFAULT::Disable);
-    spi::enable();
-
-    if false {
-        pm::enable_pba_clock(1); // SPI clock
-        let flash_spi = spi::SPI {cs: 0};
-        let flash_cs = Pin {bus: Port::PORT2, pin: 3};
-        let miso = Pin {bus: Port::PORT2, pin: 4};
-        let mosi = Pin {bus: Port::PORT2, pin: 5};
-        let sclk = Pin {bus: Port::PORT2, pin: 6};
-        let flash_attr = FlashAttr::initialize(flash_spi, flash_cs,
-                                               miso, mosi, sclk);
-
-        if flash_attr.do_attr("welcome", |c| { kputc(c as char)}) {
-            kputc('\n');
-        } else {
-            kprint("Welcome to the Tock OS!\n");
-        }
-    } */
-
     unsafe {
         task::setup();
         config::config();
-        __start_apps();
+        schedule_external_apps();
     }
-
-    // task::Task::UserTask(apps::blink::initialize as usize).post();
 
     loop {
         match unsafe { task::dequeue() } {
@@ -68,17 +63,32 @@ pub extern fn main() {
                 support::wfi(); // Sleep!
             },
             Some(task) => {
-                match task {
-                    UserTask(task_addr) => unsafe {
-                        syscall::switch_to_user(
-                            task_addr, &mut PROCESS_STACK[255]);
-                    },
-                    KernelTask(task) => {
-                        task();
-                    }
-                }
+                launch_task(task);
             }
         }
     }
 }
 
+// fn setup_spi() {
+//     use drivers::flash_attr::FlashAttr;
+//     spi::set_mode(spi::MSTR::Master, spi::PS::Variable,
+//                       spi::RXFIFO::Disable, spi::MODFAULT::Disable);
+//     spi::enable();
+
+//     if false {
+//         pm::enable_pba_clock(1); // SPI clock
+//         let flash_spi = spi::SPI {cs: 0};
+//         let flash_cs = Pin {bus: Port::PORT2, pin: 3};
+//         let miso = Pin {bus: Port::PORT2, pin: 4};
+//         let mosi = Pin {bus: Port::PORT2, pin: 5};
+//         let sclk = Pin {bus: Port::PORT2, pin: 6};
+//         let flash_attr = FlashAttr::initialize(flash_spi, flash_cs,
+//                                                miso, mosi, sclk);
+
+//         if flash_attr.do_attr("welcome", |c| { kputc(c as char)}) {
+//             kputc('\n');
+//         } else {
+//             kprint("Welcome to the Tock OS!\n");
+//         }
+//     }
+// }
